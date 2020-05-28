a057ae2de22934c462d1d57d2a3530e5138ddd32
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-05-28 20:16:35 -0300

    vamo que vamo

--- exer12_1/exer12_1-app/src/App.js
+++ exer12_1/exer12_1-app/src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+const compromissos = ['ganharLoteria', 'ViajarBrasil', 'ViajarAfrica', 'ViajarAsia', 'ViajarEuropa'];
+const tarefas = compromissos.map(tarefa => task(tarefa))
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      {task('comprar bolo de aniverário')}
+      <ul>{compromissos.map(compromissos => <li>{compromissos}</li>)}</ul> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +28,7 @@ function App() {
         >
           Learn React
         </a>
+        {tarefas}
       </header>
     </div>
   );
