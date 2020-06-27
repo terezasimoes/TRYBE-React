a6d51b6a6bc46a126679aa04b18bb5ee0e78a798
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-06-27 20:39:34 -0300

    exercicio 1 de rotas

--- exer13_3/rotas/src/App.js
+++ exer13_3/rotas/src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import rotas from './rotas'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+//Importar as páginas
+import Home from './Home';
+import About from './About';
+import Users from './Users';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Link to="/" > Home </Link>
+      <Link to="/about" > About </Link>
+      <Link to="/users" > Users </Link>
+      <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/users" component={Users} />
+      </Switch>        
+  </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
+// O componente Switch é usado para encapsular um conjunto de rotas que você definiu via Route
+//<Switch>
+{/* <Route path="/about" component={About} />
+<Route path="/contact" component={Contact} />
+<Route path="/movies" component={Movies} />
+<Route path="/" component={Home} />
+</Switch>
+ */}
